0a6649a936d9d7daf29693ecae1d791fa33a8cc3
zaikh847 <zaikh847@student.liu.se>
2022-05-22 13:48:56 +0500

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -24,9 +24,11 @@ import {
 } from "./pages";
 
 import "./App.css";
+import { useStateContext } from './contexts/ContextProvider';
 
 function App() {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
+  
   return (
     <div>
       <BrowserRouter>
